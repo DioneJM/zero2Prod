e848a668344187bae922a96b9f49952b2bed7fb9
Dione <dionemorales@outlook.com>
2022-01-09 22:49:28 +1100

    added project config

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::net::TcpListener;
 use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
-    run(address)?.await
+    let config = get_configuration().expect("Failed to read config file");
+    let address = format!("127.0.0.1:{port}", port = config.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
