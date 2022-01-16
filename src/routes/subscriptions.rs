305f4c7382166d41eaa24962ff0453c173aa2cc8
Dione <dionemorales@outlook.com>
2022-01-16 15:37:25 +1100

    refactored subscriber information to be encapsulated in NewSubscriber struct

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -4,6 +4,7 @@ use uuid::Uuid;
 
 use crate::FormData;
 use crate::startup::DbConnectionKind;
+use crate::domain::{SubscriberName, NewSubscriber};
 
 #[tracing::instrument(
     name = "Adding a new subscriber",
@@ -17,18 +18,23 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     connection: web::Data<DbConnectionKind>, // connection is passed from application state
 ) -> impl Responder {
+    let subscriber_name = SubscriberName::parse(form.name.clone());
+    let new_subscriber: NewSubscriber = NewSubscriber {
+        email: form.email.clone(),
+        name: subscriber_name
+    };
     let request_id = Uuid::new_v4();
     let request_span = tracing::info_span!(
         "Adding a new subscriber",
         %request_id,
-        subscriber_email = %form.email,
-        subscriber_name = %form.name
+        subscriber_email = %new_subscriber.email,
+        subscriber_name = %new_subscriber.name.as_ref()
     );
 
     let _request_span_guard = request_span.enter();
     tracing::info!("Saving new subscriber details - name: {} email: {}", form.name, form.email);
 
-    match insert_subscriber(&connection, &form).await {
+    match insert_subscriber(&connection, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_e) => HttpResponse::InternalServerError().finish()
     }
@@ -36,11 +42,11 @@ pub async fn subscribe(
 
 #[tracing::instrument(
     name = "Saving new subscriber in DB",
-    skip(form, connection),
+    skip(new_subscriber, connection),
 )]
 pub async fn insert_subscriber (
     connection: &DbConnectionKind,
-    form: &FormData
+    new_subscriber: &NewSubscriber
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
@@ -48,8 +54,8 @@ pub async fn insert_subscriber (
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email,
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
         .execute(connection)
