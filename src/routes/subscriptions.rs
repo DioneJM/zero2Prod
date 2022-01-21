b96ef1d03b97861b4db37869a22851e228fefa61
Dione <dionemorales@outlook.com>
2022-01-22 10:48:28 +1100

    refactored confirmation email logic

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -39,22 +39,30 @@ pub async fn subscribe(
         return HttpResponse::InternalServerError().finish();
     }
 
-    let confirmation_link = "https://my-api.com/subscriptions/confirm";
-
-    if email_client.send_email(
-        new_subscriber.email,
-        "Welcome!",
-        &format!(
-            "<h1>Welcome</h1><br/>Welcome to our newsletter! Click <a href=\"{url}\">here</a> to confirm your subscription.",
-            url = confirmation_link
-        ),
-        &format!("Welcome to our newsletter!\nVisit {url} to confirm your subscription", url = confirmation_link),
-    )
-        .await
-        .is_err() {
+    if send_confirmation_email(&email_client, new_subscriber).await.is_err() {
         return HttpResponse::InternalServerError().finish();
     }
+
     HttpResponse::Ok().finish()
+}
+
+pub async fn send_confirmation_email(
+    email_client: &EmailClient,
+    new_subscriber: NewSubscriber,
+) -> Result<(), reqwest::Error> {
+    let confirmation_link = "https://my-api.com/subscriptions/confirm";
+    let html_body = &format!(
+        "<h1>Welcome</h1><br/>Welcome to our newsletter! Click <a href=\"{url}\">here</a> to confirm your subscription.",
+        url = confirmation_link
+    );
+    let text_body = &format!("Welcome to our newsletter!\nVisit {url} to confirm your subscription", url = confirmation_link);
+    email_client.send_email(
+        new_subscriber.email,
+        "Welcome!",
+        html_body,
+        text_body,
+    )
+        .await
 }
 
 #[tracing::instrument(
