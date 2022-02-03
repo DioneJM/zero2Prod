1bba8bce63c3696859e126032bd9aa2a2f12876c
Dione <dionemorales@outlook.com>
2022-02-03 13:17:42 +1100

    added error messages to login form

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,8 +1,48 @@
-use actix_web::HttpResponse;
+use actix_web::{HttpResponse, web};
 use actix_web::http::header::ContentType;
 
-pub async fn login_form() -> HttpResponse {
+#[derive(serde::Deserialize)]
+pub struct QueryParams {
+    error: Option<String>
+}
+
+pub async fn login_form(params: web::Query<QueryParams>) -> HttpResponse {
+    let error_message = match params.0.error {
+        Some(error) => String::from(error),
+        None => "".to_string()
+    };
     HttpResponse::Ok()
         .content_type(ContentType::html())
-        .body(include_str!("login.html"))
+        .body(format!(
+            r#"
+            <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Login</title>
+</head>
+<body>
+    {error}
+    <form action="/login" method="post">
+        <label>
+            Username
+            <input type="text"
+                   placeholder="Enter Username"
+                   name="username"
+            >
+        </label>
+        <label>
+            Password
+            <input type="password"
+                   placeholder="Enter Password"
+                   name="password"
+            >
+        </label>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
+            "#,
+            error = error_message
+        ))
 }
